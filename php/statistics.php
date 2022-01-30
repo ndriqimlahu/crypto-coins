745a3a5f74d0478082427908d2c0cd4ef908b489
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-30 21:21:04 +0100

    Added the all required links inside head tag

--- php/statistics.php
+++ php/statistics.php
@@ -16,6 +16,36 @@
   <meta name="author" content="Ndriçim Lahu" />
 
   <title>Crypto Coins App</title>
+
+  <!-- Favicons -->
+  <link href="../assets/favicon/android-chrome-192x192.png" rel="android-chrome-icon" />
+  <link href="../assets/favicon/android-chrome-512x512.png" rel="android-chrome-icon" />
+  <link href="../assets/favicon/apple-touch-icon.png" rel="apple-touch-icon" />
+  <link href="../assets/favicon/favicon-16x16.png" rel="icon" />
+  <link href="../assets/favicon/favicon-32x32.png" rel="icon" />
+  <link href="../assets/favicon/favicon.ico" rel="icon" />
+
+  <!-- Icon Pack -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+  <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"></script>
+
+  <!-- Font Pack -->
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+
+  <!-- CSS -->
+  <link href="../css/statistics.css" rel="stylesheet" />
+  <link href="../css/styles.css" rel="stylesheet" />
+
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  <!-- JavaScript -->
+  <script src="../js/scripts.js"></script>
+
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
