29318f43509b28eebb2f031a3c6a83928d196c1e
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-30 21:31:44 +0100

    Added the navigation menu for News page

--- php/news.php
+++ php/news.php
@@ -49,7 +49,20 @@
 </head>
 
 <body>
-  <!-- content -->
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light navbar-custom mb-5">
+    <div class="container px-5">
+      <a class="navbar-brand text-white" href="../index.html">Crypto Coins App</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link text-white" href="../index.html">Home</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="./statistics.php">Statistics</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="./news.php">News</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </body>
 
 </html>
