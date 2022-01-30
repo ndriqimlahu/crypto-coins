d123b2ad40d27f78422266567661019f5dc736fb
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-30 21:19:04 +0100

    Created the statistics.php file inside php folder and included some lines of code

---
+++ php/statistics.php
@@ -0,0 +1,25 @@
+<?php
+  session_set_cookie_params(0);
+  session_start();
+  require "./api_coins.php";
+  require "./crypto_list.php";
+  require "./no_crypto_list.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Meta Data -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="title" content="Crypto Coins App" />
+  <meta name="author" content="Ndriçim Lahu" />
+
+  <title>Crypto Coins App</title>
+</head>
+
+<body>
+  <!-- content -->
+</body>
+
+</html>
