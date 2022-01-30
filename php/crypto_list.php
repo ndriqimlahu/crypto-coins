b5e6f1d72ccda1fa48f2c0ce2c371df4c41d2670
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-30 21:12:13 +0100

    Added the table body with data from API

--- php/crypto_list.php
+++ php/crypto_list.php
@@ -34,6 +34,45 @@
         </table>
       </div>
     </div>
+    <?php
+    foreach ($cryptoData as $crypto) {
+    ?>
+      <div class="container border border-dark rounded-3 px-5 overlay-element">
+        <div class="table-responsive">
+          <table class="table table-borderless">
+            <colgroup>
+              <col style="width: 6%;min-width:auto;max-width:auto;">
+              <col style="width: 16%;min-width:auto;max-width:auto;">
+              <col style="width: 12%;min-width:auto;max-width:auto;">
+              <col style="width: 10%;min-width:auto;max-width:auto;">
+              <col style="width: 12%;min-width:auto;max-width:auto;">
+              <col style="width: 14%;min-width:auto;max-width:auto;">
+              <col style="width: 4%;min-width:auto;max-width:auto;">
+              <col style="width: 4%;min-width:auto;max-width:auto;">
+              <col style="width: 4%;min-width:auto;max-width:auto;">
+            <tbody>
+              <tr class="h6 small text-white text-center align-middle">
+                <td><?php echo $crypto->rank ?></td><br>
+                <td><a href="<?php echo $crypto->websiteUrl ?>" target="_blank">
+                    <img src="<?php echo $crypto->icon ?>" width="25" height="25">
+                    <?php echo $crypto->name ?> (<?php echo $crypto->symbol ?>)</a></td>
+                <td>$<?php echo $crypto->price ?></td>
+                <td>$<?php echo $crypto->volume ?></td>
+                <td>$<?php echo $crypto->marketCap ?></td>
+                <td><?php echo $crypto->availableSupply ?> <?php echo $crypto->symbol ?></td>
+                <td><?php echo $crypto->priceChange1h ?>%</td>
+                <td><?php echo $crypto->priceChange1d ?>%</td>
+                <td><?php echo $crypto->priceChange1w ?>%</td>
+              </tr>
+            </tbody>
+            </colgroup>
+          </table>
+        </div>
+      </div>
+  <?php
+    }
+    ?>
+  <br><br><br><br>
   <?php
   }
 ?>
