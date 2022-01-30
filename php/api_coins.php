92e4164939e3d2558b051895d8ae70ff81f8ae5b
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-30 20:59:29 +0100

    Added the read, decode and fetch data from JSON file

--- php/api_coins.php
+++ php/api_coins.php
@@ -3,4 +3,13 @@
 
   // API Endpoint
   $apiEndpoint = "https://api.coinstats.app/public/v1/coins";
+
+  // Read JSON file
+  $jsonData = file_get_contents($apiEndpoint);
+
+  // Decode JSON data into array
+  $responseData = json_decode($jsonData);
+
+  // All crypto data exists in 'coins' object
+  $cryptoData = $responseData->coins;
 ?>
