6c6288b8b1e13377ab2faeb2a14e266d04143192
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-31 15:11:02 +0100

    Created the required functions for scroll in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+// Reloading the data
 function reloadDataFromApi() {
   window.location.reload(true);
+}
+
+// Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the page, then show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, then scroll to the top of the page
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
