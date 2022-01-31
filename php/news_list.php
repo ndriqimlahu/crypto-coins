4ce3d26b7e1d8e9828c4c626287de9b1c7a614da
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-31 14:22:53 +0100

    Created a card for news item with data from API

--- php/news_list.php
+++ php/news_list.php
@@ -12,6 +12,32 @@
         </div>
       </div>
     <?php
+    foreach ($newsData as $news) {
+    ?>
+      <br><br>
+      <div class="container d-flex justify-content-around">
+        <div class="row">
+          <div class="col-4">
+            <div class="card rounded-3" style="width: 50rem;">
+              <img class="card-img-top" src="<?php echo $news->imgURL ?>" style="height: 25rem;" alt="News Image">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $news->title ?></h5>
+                <h6 class="card-subtitle mb-3 text-muted"><cite title="Source Title"><?php echo $news->source ?></cite></h6>
+                <p class="card-text"><?php echo $news->description ?></p>
+              </div>
+              <div class="card-body">
+                <a class="btn btn-dark" href="<?php echo $news->link ?>" target="_blank" class="card-link">See More</a>
+                <a class="btn btn-dark mx-2" href="<?php echo $news->shareURL ?>" target="_blank" class="card-link">Shared Link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php
+    }
+    ?>
+    <br><br><br><br>
+    <?php
   }
 
   function newsEmptyFromApiNews($newsData) {
