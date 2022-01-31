4264fe49f6b4fdf819ec87be91069e95ef72dfeb
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-31 14:22:23 +0100

    Created a card header for Latest News

--- php/news_list.php
+++ php/news_list.php
@@ -2,7 +2,16 @@
   require "./api_news.php";
 
   function newsDataFromApiNews($newsData) {
-    //code
+    ?>
+    <br><br>
+      <div class="container text-center">
+        <div class="card bg-secondary rounded-3">
+          <div class="card-header h4 pt-2 text-white align-middle" style="height: 6rem;"><br>
+            Latest News
+          </div>
+        </div>
+      </div>
+    <?php
   }
 
   function newsEmptyFromApiNews($newsData) {
