b6c182941fe18b2c6f38ee2c27ba60814a2932c5
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-02-03 14:49:56 +0100

    Added the link for cookies integration

--- php/statistics.php
+++ php/statistics.php
@@ -39,6 +39,10 @@
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Cookies -->
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css">
+  <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
 </head>
 
 <body>
