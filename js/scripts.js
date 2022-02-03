4b7e80fbedbf348f780d5d9ff0569814ce736759
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-02-03 14:49:17 +0100

    Added an alert about cookies in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,27 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Show an alert to notify the user about the website which use cookies
+window.addEventListener("load", function() {
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#6C757D"
+      },
+      "button": {
+        "background": "#0B5ED7"
+      },
+    },
+    content: {
+      header: 'Cookies used on the website!',
+      message: 'This website uses cookies to ensure you get the best experience on our website.',
+      dismiss: 'Got it!',
+      allow: 'Allow cookies',
+      deny: 'Decline',
+      link: 'Learn more',
+      href: 'https://www.learn-about-cookies.com',
+      close: '&#x274c;',
+    }
+  })
+});
