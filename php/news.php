9de50ff4a960904ff9bea56bf59dbc414116977b
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-08-13 19:11:34 +0200

    Added the auto refresh for pages

--- php/news.php
+++ php/news.php
@@ -10,6 +10,7 @@
 <head>
   <!-- Meta Data -->
   <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="30">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="title" content="Crypto Coins" />
   <meta name="author" content="Ndriçim Lahu" />
